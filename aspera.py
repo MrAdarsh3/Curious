070e96baeca9060094c842d8f58a0cd83e0c275f
Adarsh <154623889+MrAdarsh3@users.noreply.github.com>
2025-10-01 16:25:56 +0000

    Starting

--- aspera.py
+++ aspera.py
@@ -1,1 +1,13 @@
-import this
+from num2words import num2words
+
+def number_to_word():
+    """Takes a numbef from the user & prints its word form."""
+    try:
+        user_input = int(input("Enter a number: "))
+        word_form = num2words(user_input)
+        print(f"Word Form: {word_form.title()}.")
+    except ValueError:
+        print("INVALID, Please enter a valid number!")
+
+while True:
+    number_to_word()
